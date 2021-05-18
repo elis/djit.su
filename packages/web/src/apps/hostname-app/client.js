cdd22ec42bc11d86509588fcc735bb5167830fdb
Eli Sklar <eli.sklar@gmail.com>
2021-05-19 00:38:13 +0300

    add support for co.il domains

--- packages/web/src/apps/hostname-app/client.js
+++ packages/web/src/apps/hostname-app/client.js
@@ -4,7 +4,7 @@ import HostnameApp from './app'
 
 export const name = 'Hostname App'
 
-export const check = /^([^.]+)[.]((djit[.]me)|(djitapp.local)|(works|org|com|camp))$/i
+export const check = /^([^.]+)[.]((djit[.]me)|(djitapp.local)|(works|org|com|camp|(co\.)?il))$/i
 
 export const app = (cr) => {
   console.log('Readying Hostname app!', cr)
