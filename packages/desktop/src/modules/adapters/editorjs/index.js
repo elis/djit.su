dcb208be3313ca01c572c456eac4dd46bc164d71
Rick Gove <63856101+RickGove@users.noreply.github.com>
2021-08-18 18:24:59 +0300

    Theme now changes on notebook

--- packages/desktop/src/modules/adapters/editorjs/index.js
+++ packages/desktop/src/modules/adapters/editorjs/index.js
@@ -1,14 +1,39 @@
-import React, { useCallback, useMemo, useRef } from 'react'
+import React, { useCallback, useMemo, useRef, useEffect, useState } from 'react'
 import EditorJS from 'react-editor-js'
 import { BlockType } from '#modules/core/schema/block'
 import { liveCodeTool } from '#modules/mods/react-tools/live-code-tool'
 import editorTools from './editor-tools'
+import { cloneDeep, debounce } from 'lodash'
 
 /**
  * @type {React.Component<EditorJSAdapterProps>}
  */
 const EditorJSAdapter = ({ data, toolProps, onReady }) => {
   const editorRef = useRef()
+
+  const [oldTheme, setOldTheme] = useState(toolProps.getTheme())
+  const [currentTheme, setCurrentTheme] = useState(toolProps.getTheme())
+  const [isUpdating, setIsUpdating] = useState(false)
+  const [themeChanged, setThemeChanged] = useState(false)
+
+  const [debouncedChanges, setDebouncedChanges] = useState(null)
+
+  useEffect(() => {
+    const newCurrentTheme = toolProps.getTheme()
+    if (newCurrentTheme === oldTheme || themeChanged) return
+
+    setThemeChanged(true)
+    setIsUpdating(true)
+  })
+
+  useEffect(() => {
+    if (isUpdating) {
+      setIsUpdating(false)
+      setThemeChanged(false)
+      setOldTheme(toolProps.getTheme())
+    }
+  })
+
   // const source
   const componentTools = useMemo(
     () => ({
@@ -31,18 +56,32 @@ const EditorJSAdapter = ({ data, toolProps, onReady }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  const handleChange = args => {
+    console.log(args)
+    if (cloneDeep(debouncedChanges) !== cloneDeep(args)) {
+      console.log('changes found')
+      setDebouncedChanges(args)
+    } else {
+      return
+      console.log('HERE')
+      console.log('📝', 'editor changed!', ...args)
+    }
+  }
+
   return (
-    <EditorJS
-      data={data}
-      tools={{
-        ...componentTools,
-        ...editorTools
-      }}
-      onChange={(...args) => {
-        console.log('📝', 'editor changed!', args)
-      }}
-      onReady={editorReadyHandler}
-    />
+    <>
+      {!isUpdating ? (
+        <EditorJS
+          data={data}
+          tools={{
+            ...componentTools,
+            ...editorTools
+          }}
+          // onChange={debounce(handleChange)}
+          onReady={editorReadyHandler}
+        />
+      ) : null}
+    </>
   )
 }
 
